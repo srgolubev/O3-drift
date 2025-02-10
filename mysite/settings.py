135d64de0f96cb11a9a43a80124f30dd8f725ae7
mDuck <mDuck@tuta.io>
2025-02-10 18:06:58 +0300

    feat: add accounts app to INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'rest_framework',
+    'accounts.apps.AccountsConfig',
 ]
 
 AUTH_USER_MODEL = 'core.CustomUser'
